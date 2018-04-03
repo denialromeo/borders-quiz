900a71228ce9d5936d1c2a9b48349ab83dbf1f68
Daniel <hi@danielmoore.us>
2018-04-03 15:18:27 -0700

    Made country_neighbors_dict a parameter instead of a global variable.

--- world_borders_quiz.py
+++ world_borders_quiz.py
@@ -1,7 +1,7 @@
 import json, random
 
 # bfs stands for "breadth-first search". Google this if unfamiliar.
-def bfs(country):
+def bfs(country, country_neighbors_dict):
     visited = {country: 0}
     bfs_queue = [country]
     while bfs_queue != []:
@@ -22,14 +22,14 @@ def bfs(country):
                     bfs_queue.append(neighbor)
     return visited
 
-def question(country, difficulty):
+def question(country, country_neighbors_dict, difficulty):
     s = "\nWhich of these countries does not border {0}?\n\n".format(country)
 
     possible_wrong_answers = country_neighbors_dict[country]
     if (len(possible_wrong_answers) > 3):
         possible_wrong_answers = random.sample(possible_wrong_answers, 3)
 
-    distances = bfs(country)
+    distances = bfs(country, country_neighbors_dict)
 
     if difficulty == 'easy':
         answer = random.choice(list(filter(lambda c: distances[c] >= 4, distances)))
@@ -46,14 +46,15 @@ def question(country, difficulty):
 
     return s
 
-def load_data():
+def country_neighbors_dict():
     with open('borders.json') as data_file:
         data = json.load(data_file)
         return data['land']
 
 if __name__ == '__main__':
-        country_neighbors_dict = load_data()
-        while True:
-            print(question(random.choice(list(country_neighbors_dict)), 'hard'))
-            print('Hit "Enter" for new question. Enter Ctrl + Z to exit.', end='')
-            input()
+    country_neighbors_dict = country_neighbors_dict()
+    while True:
+        random_country = random.choice(list(country_neighbors_dict))
+        print(question(random_country, country_neighbors_dict, 'hard'))
+        print('Hit "Enter" for new question. Enter Ctrl + Z to exit.', end='')
+        input()
