dd353f930398d0ab2284b5c625e9234460b0eb2e
Daniel <hi@danielmoore.us>
2018-04-03 13:43:09 -0700

    Removed all non-essential functions, put game in loop.

--- world-borders-quiz.py
+++ world-borders-quiz.py
@@ -1,22 +1,12 @@
 import json, random
 
-def random_country():
-    countries = list(data['continental']['old-world']) + list(data['continental']['new-world'])
-    return random.choice(countries)
-
-def neighbors(country):
-    if country in data['continental']['new-world']:
-        return data['continental']['new-world'][country]
-    else:
-        return data['continental']['old-world'][country]
-
 # bfs stands for "breadth-first search". Google this if unfamiliar.
 def bfs(country):
     visited = {country: 0}
     bfs_queue = [country]
     while bfs_queue != []:
         v = bfs_queue.pop(0)
-        for neighbor in neighbors(v):
+        for neighbor in countries_and_neighbors[v]:
             if neighbor not in visited:
                 visited[neighbor] = visited[v] + 1
                 # Everything borders China and Russia, skewing quiz difficulty.
@@ -30,16 +20,16 @@ def bfs(country):
 def question(country, difficulty):
     s = "\nWhich of these countries does not border {0}?\n\n".format(country)
 
-    possible_wrong_answers = neighbors(country)
+    possible_wrong_answers = countries_and_neighbors[country]
     if (len(possible_wrong_answers) > 3):
         possible_wrong_answers = random.sample(possible_wrong_answers, 3)
 
-    a = bfs(country)
+    distances = bfs(country)
 
     if difficulty == 'easy':
-        answer = random.choice(list(filter(lambda c: a[c] >= 4, a)))
+        answer = random.choice(list(filter(lambda c: distances[c] >= 4, distances)))
     if difficulty == 'hard':
-        answer = random.choice(list(filter(lambda c: a[c] == 2, a)))
+        answer = random.choice(list(filter(lambda c: distances[c] == 2, distances)))
 
     choices = possible_wrong_answers + [answer]
     random.shuffle(choices)
@@ -52,3 +42,8 @@ def question(country, difficulty):
 if __name__ == '__main__':
     with open('borders.json') as data_file:
         data = json.load(data_file)
+        countries_and_neighbors = data['continental']
+        while True:
+            question(random.choice(list(countries_and_neighbors)), 'hard')
+            print('Press "Enter" key for next question.', end='')
+            input()
