1da6380d499062305149f1738779195908e99684
Daniel <hi@danielmoore.us>
2018-04-03 13:22:27 -0700

    Removed countries_x_away methods, now only perform bfs one time.

--- world-borders-quiz.py
+++ world-borders-quiz.py
@@ -12,13 +12,13 @@ def neighbors(country):
 
 # bfs stands for "breadth-first search". Google this if unfamiliar.
 def bfs(country):
+    visited = {country: 0}
     bfs_queue = [country]
-    visited = {country: (0,'')}
     while bfs_queue != []:
         v = bfs_queue.pop(0)
         for neighbor in neighbors(v):
             if neighbor not in visited:
-                visited[neighbor] = (visited[v][0] + 1, v)
+                visited[neighbor] = visited[v] + 1
                 # Everything borders China and Russia, skewing quiz difficulty.
                 keep_china_russia = ['Finland', 'Sweden', 'Norway', 'Mongolia', 'North Korea', 'South Korea']
                 if neighbor in ['China', 'Russia'] and country not in keep_china_russia:
@@ -27,23 +27,6 @@ def bfs(country):
                     bfs_queue.append(neighbor)
     return visited
 
-def path(start_country, end_country):
-    path = [end_country]
-    a = bfs(start_country)
-    predecessor = a[end_country][1]
-    while predecessor != '':
-        path.insert(0, predecessor)
-        predecessor = a[predecessor][1]
-    return ', '.join(path)
-
-def countries_x_to_y_countries_away(start_country, x, y):
-    a = bfs(start_country)
-    return list(filter(lambda c: a[c][0] >= x and a[c][0] <= y, a))
-
-def countries_x_or_more_countries_away(start_country, x):
-    a = bfs(start_country)
-    return list(filter(lambda c: a[c][0] >= x, a))
-
 def question(country, difficulty):
     s = "\nWhich of these countries does not border {0}?\n\n".format(country)
 
@@ -51,10 +34,12 @@ def question(country, difficulty):
     if (len(possible_wrong_answers) > 3):
         possible_wrong_answers = random.sample(possible_wrong_answers, 3)
 
+    a = bfs(country)
+
     if difficulty == 'easy':
-        answer = random.choice(countries_x_or_more_countries_away(country, 4))
+        answer = random.choice(list(filter(lambda c: a[c] >= 4, a)))
     if difficulty == 'hard':
-        answer = random.choice(countries_x_to_y_countries_away(country, 2, 2))
+        answer = random.choice(list(filter(lambda c: a[c] == 2, a)))
 
     choices = possible_wrong_answers + [answer]
     random.shuffle(choices)
