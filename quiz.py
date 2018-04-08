3235d557ff412ca91d5aa6ee12be440dd6b9190a
Daniel <hi@danielmoore.us>
2018-04-07 19:22:58 -0700

    Now use territory to refer to any state or country.

--- quiz.py
+++ quiz.py
@@ -7,11 +7,11 @@ def get_args():
     parser.add_argument('--restrict-to', help='Pass in territories to ask questions for as comma-delimited string.')
     return parser.parse_args(sys.argv[1:])
 
-def open_google_maps(state):
-    l = '+'.join("https://www.google.com/maps/search/{0}".format(state).strip().split(' ')).replace("'","")
+def open_google_maps(territory):
+    l = '+'.join("https://www.google.com/maps/search/{0}".format(territory).strip().split(' ')).replace("'","")
     subprocess.run(['powershell', 'start-process', l])
 
-def state_neighbors_dict():
+def territory_neighbors_dict():
     with open('borders.json') as data_file:
         data = json.load(data_file)
         args = get_args()
@@ -24,36 +24,35 @@ def state_neighbors_dict():
         else: # Default behavior
             return { **data['countries'], **data['states'] }
 
-def random_state(state_neighbors_dict=state_neighbors_dict()):
-    return random.choice(list(state_neighbors_dict))
+def random_territory(territory_neighbors_dict=territory_neighbors_dict()):
+    return random.choice(list(territory_neighbors_dict))
 
-def exclude_neighbor_from_search(state, neighbor):
+def exclude_neighbor_from_search(territory, neighbor):
     # China and Russia make the "graph distance" difficulty mechanic a little pointless.
     # If India and Poland are just three countries apart (India → China → Russia → Poland),
     # a "hard" question asking if they border each other is a bit too easy.
     #
     # So China and Russia are removed from graph searches except when started from countries which
     # exclusively border China and/or Russia.
-    keep_china_russia = ['Finland', 'Sweden', 'Norway', 'Mongolia', 'North Korea', 'South Korea']
-    return neighbor in ['China', 'Russia'] and state not in keep_china_russia
+    if neighbor in ['China', 'Russia']:
+        keep_china_russia = ['Finland', 'Sweden', 'Norway', 'Mongolia', 'North Korea', 'South Korea']
+        return territory not in keep_china_russia
 
 # bfs stands for "breadth-first search". Google this if unfamiliar.
-def bfs(state, state_neighbors_dict=state_neighbors_dict()):
-    state_distance_dict = {state: 0}
-    bfs_queue = collections.deque([state])
+def bfs(territory, territory_neighbors_dict=territory_neighbors_dict()):
+    territory_distance_dict = {territory: 0}
+    bfs_queue = collections.deque([territory])
     while bfs_queue:
         v = bfs_queue.popleft()
-        for neighbor in state_neighbors_dict[v]:
-            if neighbor not in state_distance_dict:
-                state_distance_dict[neighbor] = state_distance_dict[v] + 1
-                if not exclude_neighbor_from_search(state, neighbor):
+        for neighbor in territory_neighbors_dict[v]:
+            if neighbor not in territory_distance_dict:
+                territory_distance_dict[neighbor] = territory_distance_dict[v] + 1
+                if not exclude_neighbor_from_search(territory, neighbor):
                     bfs_queue.append(neighbor)
-    return state_distance_dict
+    return territory_distance_dict
 
-def question(state, difficulty, state_neighbors_dict=state_neighbors_dict()):
-
-    # Trailing spaces are stripped because Georgia the country is "Georgia" while Georgia the state is "Georgia ".
-    possible_wrong_answers = [w.strip() for w in state_neighbors_dict[state]]
+def question(territory, difficulty, territory_neighbors_dict=territory_neighbors_dict()):
+    possible_wrong_answers = [w.strip() for w in territory_neighbors_dict[territory]]
     num_wrong_answers = 3
     if (len(possible_wrong_answers) > num_wrong_answers):
         wrong_answers = [w for w in random.sample(possible_wrong_answers, num_wrong_answers)]
@@ -61,37 +60,37 @@ def question(state, difficulty, state_neighbors_dict=state_neighbors_dict()):
         wrong_answers = possible_wrong_answers
     unchosen_wrong_answers = [w for w in possible_wrong_answers if w not in wrong_answers]
 
-    state_distance_dict = bfs(state, state_neighbors_dict)
+    territory_distance_dict = bfs(territory, territory_neighbors_dict)
     possible_answers = None
     if difficulty == 'easy':
-        possible_answers = [state for state in state_distance_dict if state_distance_dict[state] >= 4]
+        possible_answers = [territory for territory in territory_distance_dict if territory_distance_dict[territory] >= 4]
     if difficulty == 'hard':
-        possible_answers = [state for state in state_distance_dict if state_distance_dict[state] == 2]
+        possible_answers = [territory for territory in territory_distance_dict if territory_distance_dict[territory] == 2]
     if not possible_answers:
-        possible_answers = list(state_neighbors_dict)
+        possible_answers = list(territory_neighbors_dict)
     answer = random.choice(possible_answers).strip()
 
     choices = wrong_answers + [answer]
     random.shuffle(choices)
 
-    s = '\nWhich of these does not border {0}?\n\n'.format(state.strip())
+    s = '\nWhich of these does not border {0}?\n\n'.format(territory.strip())
 
     for idx, choice in enumerate(choices):
         s += '\t{0}. {1}\n'.format(chr(idx + 65), choice)
 
     s += '\nThe answer is {0}. {1}\n'.format(chr(choices.index(answer) + 65), answer)
 
-    s += '\n{0} also borders {1}\n'.format(state.strip(), unchosen_wrong_answers)
+    s += '\n{0} also borders {1}\n'.format(territory.strip(), unchosen_wrong_answers)
 
     return s
 
 if __name__ == '__main__':
     args = get_args()
     while True:
-        state = random_state() if not args.restrict_to else random.choice(args.restrict_to.split(','))
-        print(question(state, 'hard'))
-        i = input('Hit "Enter" for new question. Enter "m" to see {0} on Google Maps. "q" to quit.'.format(state.strip()))
+        territory = random_territory() if not args.restrict_to else random.choice(args.restrict_to.split(','))
+        print(question(territory, 'hard'))
+        i = input('Hit "Enter" for new question. Enter "m" to see {} on Google Maps. "q" to quit.'.format(territory.strip()))
         if i == 'm':
-            open_google_maps(state)
+            open_google_maps(territory)
         elif i == 'q':
             break
