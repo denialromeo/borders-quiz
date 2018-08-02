9a350c9c532db4524ba6cd8cd21237a98ad49f9e
Daniel Moore <hi@danielmoore.us>
2018-08-02 10:58:57 -0700

    Removed extraneous comments from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  // webpack folder's entry js - excluded from jekll's build process, since the compiled version is what we'll use in the DOM.
   entry: "./borders-quiz/js/main.js",
   output: {
-    // put the generated file in the assets folder so jekyll will grab it.
       path: __dirname + "/js",
       filename: "bundle.js"
   }
