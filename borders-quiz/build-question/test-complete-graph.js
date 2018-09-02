5a4f7410befd207abe900988c23e8add14003117
Daniel Moore <hi@danielmoore.us>
2018-09-02 06:25:21 -0700

    Updated game to latest version.

--- borders-quiz/build-question/test-complete-graph.js
+++ borders-quiz/build-question/test-complete-graph.js
@@ -1,12 +1,13 @@
-// This simple script tests whether the adjacency lists in borders.json encode complete graphs.
-//
-// Borders are always undirected graphs, so this is a simple way of checking if we missed something.
-//
-// Example Usage: node test_complete_graph.js
 const borders = require('./borders.json')
 
 Array.prototype.contains = function(s) { return this.indexOf(s) >= 0 }
 
+/* This tests if the adjacency lists in borders.json encode complete graphs.
+ *
+ * Borders are always undirected graphs, so this is a simple way of checking if we missed something.
+ *
+ * (Example: If the United States borders Mexico but Mexico doesn't border the U.S., that's an error!)
+ */
 function is_complete_graph(quiz_mode) {
 	var vertex_neighbors_dict = borders[quiz_mode]
 	var complete = true
@@ -32,3 +33,31 @@ Object.keys(borders).forEach(function(quiz_mode) {
 	is_complete_graph(quiz_mode)
 	console.log()
 })
+
+/* This tests whether there are any duplicate keys in borders.json.
+ *
+ * (If Suffolk in England and Suffolk County in New York are both labeled Suffolk, we'll run into an error
+ *  where the England quiz randomly jumps to New York for a question!)
+ */
+function all_keys() {
+	return Object.keys(borders)
+	      .map(mode => Object.keys(borders[mode]))
+	      .reduce((arr, next_arr) => arr.concat(next_arr))
+	      .sort()
+}
+
+function duplicate_keys(keys) {
+	var duplicates = []
+	for (let i = 0; i < keys.length - 1; i++) {
+		if (keys[i] == keys[i + 1]) {
+			duplicates.push(keys[i])
+		}
+	}
+	return duplicates
+}
+
+var duplicates = duplicate_keys(all_keys())
+
+if (duplicates.length > 0) {
+	console.log(`Duplicates: ${duplicates}. Please fix!`)
+}
