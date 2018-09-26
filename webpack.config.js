71c9ee90ab315a517a01c480cb4d5aad83c5bc11
Daniel Moore <hi@danielmoore.us>
2018-09-26 15:00:30 -0700

    Updated game to latest version.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  entry: "./borders-quiz/main.js",
-  output: {
-    path: __dirname + "/js",
-    filename: "bundle.js"
-},
-module: {
-	rules: [
-	{
-		test: /\.js$/,
-		exclude: /node_modules/,
-		use: {
-			loader: 'babel-loader',
-			options: {
-				presets: ['babel-preset-env']
+	entry: "./borders-quiz/main.js",
+	output: {
+		path: __dirname + "/js",
+		filename: "borders-quiz.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['babel-preset-env']
+					}
+				}
 			}
-		}
+		]
 	}
-	]
-}
 };
