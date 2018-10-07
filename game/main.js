b0900c1585b7f8a000bc15f9e8c2144c605afa5b
Daniel Moore <hi@danielmoore.us>
2018-10-07 09:50:16 -0700

    Removed redundant index.js file.

--- game/main.js
+++
@@ -1,8 +0,0 @@
-const $ = require("jquery")
-
-var { first_question, other_quiz_modes_message } = require("./game.js")
-
-$(document).ready(function() {
-	first_question()
-})
-document.write(other_quiz_modes_message())
